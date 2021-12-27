0e267e0095a20bd22d9bab3a75b8379116c134da
Vivian Tao <vivian0tao@gmail.com>
2021-12-26 21:52:20 -0500

    installed mysql and dotenv

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 const express = require("express"),
-    app = express();
+    mysql = require("mysql"),
+    
+const app = express();
+require("dotenv").config();
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: process.env.MYSQL_PASSWORD,
+    database: "QPRConnections"
+});
+
+connection.connect((err) => {
+    if (err) {
+      return console.error('error: ' + err.message);
+    }
+    console.log('Connected to DB');
+});
 
 app.get("/", (req, res, next) => {
     res.send("test");
 });
 
 app.listen(3000, function () {
-    console.log("Running on port 3000");
+    console.log("Server Started");
   });
