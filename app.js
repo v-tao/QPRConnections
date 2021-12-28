a023086b592d88a6f7e0f0affa800395139edc19
Vivian Tao <vivian0tao@gmail.com>
2021-12-28 11:53:39 -0500

    created update profile route

--- app.js
+++ app.js
@@ -19,21 +19,47 @@ con.connect((err) => {
     console.log('Connected to DB');
 });
 
+////////// CREATE USER //////////
 app.post("/register", (req, res) => {
-    let sql = `INSERT INTO users (\`name\`, gender, interestedGenders, birthday, location, distance, about, okActions, notOkActions) VALUES("${req.body.name}", "${req.body.gender}", "${req.body.interestedGenders}", DATE("${req.body.birthday}"), POINT(${req.body.location[0]}, ${req.body.location[1]}), ${req.body.distance}, "${req.body.about}", "${req.body.okActions}", "${req.body.notOkActions}")`;
+    let sql = `
+    INSERT INTO users 
+    (\`name\`, gender, interestedGenders, birthday, location, distance, about, okActions, notOkActions) 
+    VALUES("${req.body.name}", "${req.body.gender}", "${req.body.interestedGenders}", DATE("${req.body.birthday}"), POINT(${req.body.location[0]}, ${req.body.location[1]}), ${req.body.distance}, "${req.body.about}", "${req.body.okActions}", "${req.body.notOkActions}")`;
     con.query(sql, (err, result) => {
         if(err) throw err;
         res.send("Account successfully created.")
     })
 });
 
+////////// GET USER //////////
 app.get("/users/:id", (req, res) => {
     let sql = `SELECT * FROM users WHERE id=${req.params.id}`
     con.query(sql, (err, result) => {
         if(err) throw err;
         res.json(result);
+    });
+});
+
+////////// UPDATE USER //////////
+app.put("/users/:id", (req, res) => {
+    let sql = `
+    UPDATE users
+    SET
+    \`name\`="${req.body.name}",
+    gender="${req.body.gender}",
+    interestedGenders="${req.body.interestedGenders}",
+    location=POINT(${req.body.location[0]}, ${req.body.location[1]}),
+    distance=${req.body.distance},
+    about="${req.body.about}",
+    okActions="${req.body.okActions}",
+    notOkActions="${req.body.notOkActions}"
+    WHERE id=${req.params.id}
+    `
+    con.query(sql, (err, result) => {
+        if (err) throw err;
+        res.send("Account successfully updated.");
     })
-})
+});
 
 app.listen(3000, function () {
     console.log("Server Started");
