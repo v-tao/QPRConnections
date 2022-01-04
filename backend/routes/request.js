6d66adc629e688c83b9e06cecc11827a8116b1c4
Vivian Tao <vivian0tao@gmail.com>
2022-01-03 23:42:43 -0500

    reqrote request routes to use async/await and created requestExists middleware

--- backend/routes/request.js
+++ backend/routes/request.js
@@ -2,40 +2,37 @@ const express = require("express"),
     router = express.Router();
 
 const pool = require("../pool");
-const indexMiddleware = require("../middleware/index");
-const {sentRequest, receivedRequest} = require("../middleware/request");
+const indexMiddleware = require("../middleware/index"),
+    f = indexMiddleware.asyncWrapper;
+const requestMiddleware = require("../middleware/request");
 
 router.use(indexMiddleware.isLoggedIn);
 
 ////////// GET REQUESTS SENT //////////
-router.get("/sent", (req, res) => {
+router.get("/sent", f(async (req, res) => {
     let sql = `
     SELECT * FROM user LEFT JOIN user_request 
     ON user.id=user_request.requested_user_id 
     WHERE user_request.user_id=?
     `
-    pool.query(sql, [req.user[0].id], (err, sentRequests) => {
-        if(err) throw err;
-        res.json(sentRequests);
-    });
-});
+    let sentRequests = await pool.query([req.user[0].id]);
+    res.json(sentRequests);
+}));
   
 ////////// GET REQUESTS RECEIVED //////////
-router.get("/received", (req, res) => {
+router.get("/received", f(async (req, res) => {
     // return the requests that have not already been rejected
     let sql = `
     SELECT * FROM user LEFT JOIN user_request
     ON user.id=user_request.user_id
     WHERE user_request.requested_user_id=? AND user_request.rejected=FALSE
     `
-    pool.query(sql, [req.user[0].id], (err, receivedRequests) => {
-        if (err) throw err;
-        res.json(receivedRequests);
-    });
-});
+    let receivedRequests = await pool.query(sql, [req.user[0].id]);
+    res.json(receivedRequests);
+}));
 
 ////////// DELETE REQUEST //////////
-router.delete("/:id/delete", sentRequest, (req, res) => {
+router.delete("/:id/delete", requestMiddleware.requestExists, requestMiddleware.sentRequest, (req, res) => {
     let sql = `DELETE FROM user_request WHERE id=?`
     pool.query(sql, [req.params.id], (err, request) => {
         if (err) throw err;
@@ -44,7 +41,7 @@ router.delete("/:id/delete", sentRequest, (req, res) => {
 });
 
 ////////// ACCEPT REQUEST (match) //////////
-router.post("/:id/accept", receivedRequest, (req, res) => {
+router.post("/:id/accept", requestMiddleware.requestExists, requestMiddleware.receivedRequest, f(async (req, res) => {
     // add users to match table and delete request
     let sql =  `
     INSERT INTO user_match (first_user_id, second_user_id)
@@ -53,20 +50,16 @@ router.post("/:id/accept", receivedRequest, (req, res) => {
     WHERE id=?;
     DELETE FROM user_request WHERE id=?
     `
-    pool.query(sql, [req.params.id, req.params.id], (err, match) => {
-        if (err) throw err;
-        res.send("Request successfully accepted.");
-    })
-});
+    await pool.query(sql, [req.params.id, req.params.id]);
+    res.send("Request successfully accepted");
+}));
 
 ////////// REJECT REQUEST //////////
-router.post("/:id/reject", receivedRequest, (req, res) => {
+router.post("/:id/reject", requestMiddleware.requestExists, requestMiddleware.receivedRequest, f(async (req, res) => {
     // set the rejected field of the request to true
     let sql =  `UPDATE user_request SET rejected=TRUE WHERE id=?`
-    pool.query(sql, [req.params.id], (err, request) => {
-        if (err) throw err;
-        res.send("Request successfully rejected.");
-    });
-});
+    await pool.query(sql, [req.params.id]);
+    res.send("Request successfully rejected.");
+}));
 
 module.exports = router;
